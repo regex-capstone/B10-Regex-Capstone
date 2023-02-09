f42ff31a20cc884b5d2f547f01ec77d11bb08822
Alan Wen <wenjalan@gmail.com>
2023-02-09 12:44:14 -0800

    MUI theme init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
 import { AppProps } from 'next/app'
-import './global.css'
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material'
+
+// application theme, left intentionally empty (default)
+const theme = createTheme({})
 
 /* (application root) */
 export default function App({ Component, pageProps }: AppProps<any>) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
