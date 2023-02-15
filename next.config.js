6a3d2cba5b68d004989ca04e3f6e335d1fdcee35
kje123 <keith-3@hotmail.com>
2023-02-14 22:12:52 -0800

    added basic tfidf scaffolding, fixed webpack config to skip over fs when building

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {},
-  webpack(config) {
+  webpack(config, { isServer }) {
     config.experiments = { ...config.experiments, topLevelAwait: true }
+
+    if (!isServer) {
+      // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+      config.resolve.fallback = {
+        fs: false
+      }
+    }
+
     return config
   },
 }
