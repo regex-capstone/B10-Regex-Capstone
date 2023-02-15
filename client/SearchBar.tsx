694a72bcb211aaf4783722daa4492819287ec46d
Alan Wen <wenjalan@gmail.com>
2023-02-14 16:27:51 -0800

    Added padding to SearchBar

--- client/SearchBar.tsx
+++ client/SearchBar.tsx
@@ -3,8 +3,15 @@ import { Button, Stack, TextField } from "@mui/material";
 export default function SearchBar(props: { query: string | string[] | undefined }) {
   const { query } = props;
   return (
-    <Stack direction={'row'} spacing={2}>
-      <TextField value={query} />
+    <Stack direction={'row'} spacing={2} sx={{
+      paddingTop: 2,
+      paddingBottom: 2,
+      alignItems: 'stretch',
+      justifyContent: 'start'
+    }}>
+      <TextField value={query} sx={{
+        flexGrow: 1,
+      }} />
       <Button variant="contained">Search</Button>
     </Stack>
   )
