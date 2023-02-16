77f0365b5b28d589c4b61c1a06d4dce2b56afc47
kje123 <keith-3@hotmail.com>
2023-02-16 13:51:16 -0800

    commiting current changes to rebase to main

--- client/SearchBar.tsx
+++ client/SearchBar.tsx
@@ -1,5 +1,5 @@
 import { Button, Stack, TextField } from "@mui/material";
-import Search from "@/client/Search";
+import natural from "natural";
 
 export default function SearchBar(props: { query: string | string[] | undefined }) {
   const { query } = props;
@@ -16,4 +16,25 @@ export default function SearchBar(props: { query: string | string[] | undefined 
       <Button variant="contained" onClick={Search}>Search</Button>
     </Stack>
   )
+}
+
+function Search() {
+    let testStrings = [    // create array of test strings to search
+        "the quick brown fox jumps over the lazy dog",
+        "here is how to request a course substitution",
+        "test test test",
+        "substitution is a funny word",
+        "fox is the best character in super smash brothers melee",
+        "hello world"
+    ]
+
+    let tfidf = new natural.TfIdf;  //init
+
+    for(let i = 0; i < testStrings.length; i++) {
+        tfidf.addDocument(testStrings[i]);
+    }
+
+    tfidf.tfidfs('fox and dog', function(i, measure) {
+        console.log('document #' + i + ' is ' + measure);
+    });
 }
