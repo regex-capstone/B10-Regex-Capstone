69be83c2ac9304f3365f301c6d566aa7412282b7
Alan Wen <wenjalan@gmail.com>
2023-02-20 09:29:56 -0800

    Fetch pages from API

--- pages/category/[name].tsx
+++ pages/category/[name].tsx
@@ -4,7 +4,7 @@ import APIEndpoint from "@/isaac/api/APIEndpoint"
 import { GetStaticPathsContext, GetStaticPathsResult, GetStaticPropsContext, GetStaticPropsResult } from "next"
 import SearchBar from "@/client/SearchBar"
 import Head from "next/head"
-import { Container, Stack } from "@mui/material";
+import { Box, Container, Stack } from "@mui/material";
 import Grid2 from '@mui/material/Unstable_Grid2'
 import Link from "next/link"
 
@@ -22,26 +22,25 @@ export async function getStaticPaths(context: GetStaticPathsContext): Promise<Ge
 export async function getStaticProps(context: GetStaticPropsContext): Promise<GetStaticPropsResult<CategoryProps>> {
   const api: API = APIEndpoint
   const { name } = context.params ?? {};
-  // const category: Category = await api.getCategoryByName(name as string)
-  // const pages: Page[] = await api.getCategoryPages(category.id as string)
-  const category = { name: "Example Category", id: "1", created_at: 12345 }
-  const pages: any = []
+  const category: Category = await api.getCategoryByName(name as string)
+  const pages: Page[] = await api.getPagesByCategoryId(category.id as string)
   return {
     props: {
-      category,
-      pages
+      category: JSON.stringify(category),
+      pages: JSON.stringify(pages),
     },
     revalidate: 10,
   }
 }
 
 interface CategoryProps {
-  category: Category
-  pages: Page[]
+  category: string
+  pages: string
 }
 
 export default function Directory(props: CategoryProps) {
-  const { category, pages } = props
+  const category = JSON.parse(props.category) as Category
+  const pages = JSON.parse(props.pages) as Page[]
   const query = ""
 
   return (
@@ -54,13 +53,12 @@ export default function Directory(props: CategoryProps) {
           <Grid2 xs={3}>
             <Stack direction={'column'} spacing={2}>
               <h1>ISAAC</h1>
-              {/* <ContentTable page={pageData} /> */}
             </Stack>
           </Grid2>
           <Grid2 xs={6}>
             <Stack direction={'column'} spacing={2}>
               <SearchBar initialQuery={query} />
-              <Content name={category.name} pages={[]} />
+              <Content name={category.name} pages={pages} />
             </Stack>
           </Grid2>
         </Grid2>
@@ -72,16 +70,17 @@ export default function Directory(props: CategoryProps) {
 function Content(props: { name: string, pages: Page[] }) {
   const { name, pages } = props
   return (
-    <div>
+    <Stack direction="column" spacing={2}>
       <h1>{name}</h1>
       <hr />
-      <ul>
+      <i>{pages.length} page(s) in category</i>
+      <Stack direction="column" spacing={1}>
         {pages.map((page: Page) => (
-          <li key={page.id}>
-            <Link href={`/page/${page.id}`}>{page.title}</Link>
-          </li>
+          <Box key={page.id}>
+            <Link href={`/page/${page.title}`}>{page.title}</Link>
+          </Box>
         ))}
-      </ul>
-    </div>
+      </Stack>
+    </Stack>
   )
 }
