90c92f1d7355a9f026f6c16a4ef901be551f6c5d
Alan Wen <wenjalan@gmail.com>
2023-02-23 09:56:15 -0800

    Added SessionProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,19 @@
 import { AppProps } from 'next/app'
 import { CssBaseline, ThemeProvider, createTheme } from '@mui/material'
+import { SessionProvider } from 'next-auth/react'
 
 // application theme, left intentionally empty (default)
 const theme = createTheme({})
 
 /* (application root) */
 export default function App({ Component, pageProps }: AppProps<any>) {
+  const { session } = pageProps
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Component {...pageProps} />
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
     </ThemeProvider>
   )
 }
