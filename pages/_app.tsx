8dee92459147c4fcb6653ec46176145a4618104c
Alan Wen <wenjalan@gmail.com>
2023-02-23 09:57:44 -0800

    Added auth header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,21 @@
 import { AppProps } from 'next/app'
 import { CssBaseline, ThemeProvider, createTheme } from '@mui/material'
+import { SessionProvider } from 'next-auth/react'
+import HeaderBar from '@/client/HeaderBar'
 
 // application theme, left intentionally empty (default)
 const theme = createTheme({})
 
 /* (application root) */
 export default function App({ Component, pageProps }: AppProps<any>) {
+  const { session } = pageProps
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Component {...pageProps} />
+      <SessionProvider session={session}>
+        <HeaderBar />
+        <Component {...pageProps} />
+      </SessionProvider>
     </ThemeProvider>
   )
 }
