26e2a9cb15b252b38b401fa4768122fbc6855220
Alan Wen <wenjalan@gmail.com>
2023-02-28 09:02:49 -0800

    Logo style update

--- client/Logo.tsx
+++ client/Logo.tsx
@@ -1,9 +1,22 @@
+import { Box, Typography } from "@mui/material"
 import Link from "next/link"
+import Theme from "./Theme"
 
 export default function Logo() {
   return (
-    <Link href="/">
-      <h1>ISAAC</h1>
+    <Link href="/" style={{
+      textDecoration: 'none',
+    }}>
+      <Box sx={{
+        color: Theme.COLOR.TEXT_DARK,
+        fontSize: '4rem',
+        display: 'flex',
+        alignItems: 'center',
+        textAlign: 'center',
+        letterSpacing: '1rem',
+      }}>
+        <b>ISAAC</b>
+      </Box>
     </Link>
   )
 }
