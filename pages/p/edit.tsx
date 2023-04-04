2e1dd7f6c343a767862ffd9ee8afcbdd53b86f30
Alan Wen <wenjalan@gmail.com>
2023-04-04 13:36:08 -0700

    Partial Edit Page Refactoring

--- pages/p/edit.tsx
+++ pages/p/edit.tsx
@@ -3,55 +3,62 @@ import { Container, Stack, Button } from "@mui/material";
 import Grid2 from '@mui/material/Unstable_Grid2';
 import Head from "next/head";
 import Logo from "@/client/Logo";
-import React from 'react';
-import LoadingSpinner from "@/client/LoadingSpinner";
+import { useEffect, useState } from 'react';
 import RichTextEditor from "@/client/RichTextEditor";
 import { useRouter } from "next/router";
-import usePage from "@/client/hooks/usePage";
+import { Page, Revision } from "@/isaac/models";
 
-/* (root)/ */
+/* /p/edit?page=[title] */
 export default function Edit() {
     const router = useRouter();
-    const { data, error, isLoading } = usePage(router.query.title as string);
+    const { page } = router.query;
 
-    if (isLoading) return <LoadingSpinner />
-    if (error) return <div>{error.message}</div>
+    const [pageData, setPageData] = useState<Page | undefined>(undefined);
+    const [revisionData, setRevisionData] = useState<Revision | undefined>(undefined);
 
-    const pageData = data.page;
-    const revisionData = data.revision;
-
-    const query = "";
+    useEffect(() => {
+        fetch(`/api/page/${page}`)
+            .then(response => response.json())
+            .then(data => setPageData(data))
+            .catch(() => setPageData(undefined))
+        // TODO: Somehow get the latest revision
+        // fetch(`/api/revision/some-id-here`)
+        //     .then(response => response.json())
+        //     .then(data => setRevisionData(data));
+    }, [page]);
 
     return (
         <>
             <Head>
-                <title>{`Editing ${pageData.title} | ISAAC`}</title>
+                <title>{`Editing ${page} | ISAAC`}</title>
             </Head>
-            <Container>
-                <Grid2 container spacing={2}>
-                    <Grid2 xs={3}>
-                        <Stack direction={'column'} spacing={2}>
-                            <Logo />
-                            <Button href={`/page/${pageData.title}`} sx={{
-                                width: 'fit-content'
-                            }}>
-                                Back to Page
-                            </Button>
-                        </Stack>
+            {pageData === undefined || revisionData === undefined ? <h1>Loading...</h1> :
+                <Container>
+                    <Grid2 container spacing={2}>
+                        <Grid2 xs={3}>
+                            <Stack direction={'column'} spacing={2}>
+                                <Logo />
+                                <Button href={`/page/${pageData.title}`} sx={{
+                                    width: 'fit-content'
+                                }}>
+                                    Back to Page
+                                </Button>
+                            </Stack>
+                        </Grid2>
+                        <Grid2 xs={6}>
+                            <Stack direction={'column'} spacing={2}>
+                                <SearchBar initialQuery={""} />
+                                <RichTextEditor
+                                    pageData={pageData}
+                                    revisionData={revisionData}
+                                />
+                            </Stack>
+                        </Grid2>
+                        <Grid2 xs={3}>
+                        </Grid2>
                     </Grid2>
-                    <Grid2 xs={6}>
-                        <Stack direction={'column'} spacing={2}>
-                            <SearchBar initialQuery={query} />
-                            <RichTextEditor 
-                                pageData={pageData}
-                                revisionData={revisionData}
-                            />
-                        </Stack>
-                    </Grid2>
-                    <Grid2 xs={3}>
-                    </Grid2>
-                </Grid2>
-            </Container>
+                </Container>
+            }
         </>
     )
 }
