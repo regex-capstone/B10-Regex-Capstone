311febd14e0f8513ead6248c59be3dfe99a2d7eb
Alan Wen <wenjalan@gmail.com>
2023-04-07 13:02:45 -0700

    SearchBar static

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
-import { Avatar, Box, Button, Container, Icon, IconButton, Stack } from "@mui/material";
+import { Autocomplete, AutocompleteRenderInputParams, Avatar, Box, Button, Container, Icon, IconButton, Stack, TextField, Typography } from "@mui/material";
 import AddIcon from '@mui/icons-material/Add';
 import AnalyticsIcon from '@mui/icons-material/Analytics';
+import SearchIcon from '@mui/icons-material/Search';
 import Head from "next/head";
+import { ReactNode } from "react";
+import InputUnstyled from '@mui/base/InputUnstyled';
+
 
 export default function Index() {
     return (
@@ -11,21 +15,6 @@ export default function Index() {
             </Head>
             <Header />
             <Box sx={{
-                width: "100%",
-                position: "absolute",
-                marginTop: "10rem",
-            }}>
-                <Container sx={{
-                    backgroundColor: "#FCF",
-                }}>
-                    <Stack spacing={2} direction="column">
-                        <div>Thing1</div>
-                        <div>Thing2</div>
-                        <div>Thing3</div>
-                    </Stack>
-                </Container>
-            </Box>
-            <Box sx={{
                 // outline: "1px solid red",
                 position: "relative",
                 zIndex: -1,
@@ -34,12 +23,46 @@ export default function Index() {
                 backgroundSize: "cover",
                 backgroundRepeat: "no-repeat",
                 backgroundPosition: "center",
-                filter: "blur(2px)",
                 display: "flex",
                 alignItems: "center",
                 justifyContent: "center",
+                filter: "blur(2px)",
             }} />
-
+            <Box sx={{
+                width: "100%",
+                // backgroundColor: "#FAD",
+                zIndex: 0,
+                position: "absolute",
+                top: 0,
+                left: 0,
+                marginTop: "15vh",
+            }}>
+                <Container>
+                    <Stack spacing={2} direction="column" alignItems="center">
+                        <Typography
+                            fontFamily="sans-serif"
+                            fontWeight="700"
+                            fontSize="32px"
+                            color="#FFF"
+                            letterSpacing="0.75em"
+                            sx={{
+                                textShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)"
+                            }}
+                        >
+                            ISAAC
+                        </Typography>
+                        <SearchBar />
+                        <Stack spacing={1} direction="row">
+                            <Button variant="contained">
+                                Search
+                            </Button>
+                            <Button variant="contained">
+                                Browse TOpics
+                            </Button>
+                        </Stack>
+                    </Stack>
+                </Container>
+            </Box>
             <Box sx={{
                 height: "100%",
                 backgroundColor: "#FFF",
@@ -100,6 +123,37 @@ function Header() {
                 </IconButton>
             </Stack>
         </Box>
+    )
+}
+
+function SearchBar() {
+    return (
+        <Box sx={{
+            minWidth: "60%",
+        }}>
+            <Autocomplete
+                id="free-solo-demo"
+                freeSolo
+                // TODO: Add Autosugggestions
+                options={["Suggestion 1", "Suggestion 2", "Suggestion 3"]}
+                renderInput={(params) => <TextField
+                    {...params}
+                    variant="standard"
+                    InputProps={{
+                        disableUnderline: true,
+                        endAdornment: <SearchIcon />,
+                    }}
+                    sx={{
+                        paddingLeft: "1em",
+                        paddingRight: "1em",
+                        paddingTop: "0.5em",
+                        paddingBottom: "0.5em",
+                        backgroundColor: "#FFF",
+                        borderRadius: 30,
+                        border: "none",
+                    }} />}
+            />
+        </Box >
     )
 }
 
