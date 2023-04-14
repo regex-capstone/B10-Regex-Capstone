892fab2775d5656d0210c6b2d3b31c6aa8d7defa
Alan Wen <wenjalan@gmail.com>
2023-04-13 23:47:55 -0700

    Delete unused code

--- client/Header.tsx
+++ client/Header.tsx
@@ -1,7 +1,5 @@
-import { Avatar, Box, Button, IconButton, Stack, TextField, Typography } from '@mui/material';
-import Theme from '@/client/Theme';
-import { signIn, signOut, useSession } from 'next-auth/react';
-import Link from 'next/link';
+import { Avatar, Box, IconButton, Stack, TextField } from '@mui/material';
+import { signIn, useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
 import Search from '@mui/icons-material/Search';
 import { ReactNode, useState } from 'react';
@@ -82,64 +80,5 @@ function SearchBar() {
                 width: "100%",
             }}
         />
-    )
-}
-
-function ProfileComponent() {
-    const { data: session } = useSession();
-
-    const boxStyle = {
-        // outline: "1px solid red",
-        alignItems: "center",
-        display: "flex",
-        paddingLeft: '0.5rem',
-        paddingRight: '0.5rem',
-    }
-
-    const buttonStyle = {
-        color: Theme.COLOR.SECONDARY,
-        borderRadius: 0,
-        // outline: "1px solid red",
-    }
-
-    if (!session) {
-        return (
-            <>
-                <Box sx={boxStyle}>
-                    <Typography fontSize={'1rem'}>
-                        <b>GUEST</b>
-                    </Typography>
-                </Box>
-                <Button variant="text" sx={buttonStyle}>
-                    <Box onClick={() => signIn()}>
-                        <Typography fontSize={'1rem'}>
-                            <b>Sign In</b>
-                        </Typography>
-                    </Box>
-                </Button>
-            </>
-        )
-    }
-
-    return (
-        <>
-            <Box sx={boxStyle}>
-                <Typography fontSize={'1rem'}>
-                    <Link href="/profile" style={{
-                        textDecoration: 'none',
-                        color: Theme.COLOR.TEXT_LIGHT,
-                    }}>
-                        <b>{session?.user.name.toUpperCase()}</b>
-                    </Link>
-                </Typography>
-            </Box>
-            <Button variant="text" sx={buttonStyle}>
-                <Box onClick={() => signOut()}>
-                    <Typography fontSize={'1rem'}>
-                        <b>Sign Out</b>
-                    </Typography>
-                </Box>
-            </Button>
-        </>
     )
 }
