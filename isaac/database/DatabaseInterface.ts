2a12653fe723e556bae5c3e34cea144052d89da5
treblenaX <elbertkcheng@outlook.com>
2023-04-15 22:37:43 -0700

    added documentation for the database interface

--- isaac/database/DatabaseInterface.ts
+++ isaac/database/DatabaseInterface.ts
@@ -3,24 +3,74 @@ import type Metric from '../analytics/model'
 
 export default interface Database {
     // READ
-    getPages(query: Object, sort: SortOptions): Promise<SuccessDBResponse | ErrorDBResponse>,
-    getRevisions(query: Object, sort: SortOptions): Promise<SuccessDBResponse | ErrorDBResponse>,
-    getCategories(query: Object, sort: SortOptions): Promise<SuccessDBResponse | ErrorDBResponse>,
+    /**
+     * GET pages from the database
+     * @param query filter object to apply to the query
+     * @param sort sort object to apply to the query
+     */
+    getPages(query: any, sort: any): Promise<SuccessDBResponse | ErrorDBResponse>,
+    /**
+     * GET revisions from the database
+     * @param query filter object to apply to the query
+     * @param sort sort object to apply to the query
+     */
+    getRevisions(query: any, sort: any): Promise<SuccessDBResponse | ErrorDBResponse>,
+    /**
+     * GET categories from the database
+     * @param query filter object to apply to the query
+     * @param sort sort object to apply to the query
+     */
+    getCategories(query: any, sort: any): Promise<SuccessDBResponse | ErrorDBResponse>,
 
     // CREATE
+    /**
+     * ADD a new page in the database
+     * @param page new page object to be stored
+     */
     addPage(page: Page): Promise<SuccessDBResponse | ErrorDBResponse>,
+    /**
+     * ADD a new revision in the database
+     * @param rev new revision object to be stored
+     */
     addRevision(rev: Revision): Promise<SuccessDBResponse | ErrorDBResponse>,
+    /**
+     * ADD a new category in the database
+     * @param cat new cateogory object to be stored
+     */
     addCategory(cat: Category): Promise<SuccessDBResponse | ErrorDBResponse>,
     
     // UPDATE
-    updatePage(id: string, query: Object): Promise<SuccessDBResponse | ErrorDBResponse>,
+    /**
+     * UPDATE a page in the database
+     * @param id the page id to update
+     * @param query the properties of the page to update
+     */
+    updatePage(id: string, query: any): Promise<SuccessDBResponse | ErrorDBResponse>,
     
     // DELETE
+    /**
+     * DELETE a page from the database
+     * @param id the id of the page to delete
+     */
     deletePage(id: string): Promise<SuccessDBResponse | ErrorDBResponse>,
+    /**
+     * DELETE a revision from the database
+     * @param id the id of the revision to delete
+     */
     deleteRevision(id: string): Promise<SuccessDBResponse | ErrorDBResponse>,
+    /**
+     * DELETE a category from the database
+     * @param id the id of the category to delete
+     */
     deleteCategory(id: string): Promise<SuccessDBResponse | ErrorDBResponse>,
 
     // aggregate
+    /**
+     * AGGREGATE metrics from the database
+     * @param groupOptions group options for the aggregation
+     * @param sortOptions sort options for the aggregation
+     * @param lookupOptions populate model references with the model object(s)
+     */
     aggMetrics(groupOptions: any, sortOptions: any, lookupOptions: any): Promise<SuccessDBResponse | ErrorDBResponse>,
 
     getAnalytics(query: Object): Promise<SuccessDBResponse | ErrorDBResponse>,
@@ -30,10 +80,6 @@ export default interface Database {
     getLatestUsers(query: any): Promise<SuccessDBResponse | ErrorDBResponse>,
     addNewUser(user: User): Promise<SuccessDBResponse | ErrorDBResponse>,
     updateUser(user: User): Promise<SuccessDBResponse | ErrorDBResponse>
-}
-
-export interface SortOptions {
-    created_at?: number
 }
 
 interface SuccessDBResponse {
