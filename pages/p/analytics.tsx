b985e6acb618540831aa5775672c331b59acf98a
kje123 <keith-3@hotmail.com>
2023-04-21 19:03:33 -0700

    add component icon is interactable (not functional)

--- pages/p/analytics.tsx
+++ pages/p/analytics.tsx
@@ -1,14 +1,11 @@
-import { Button, Container, Stack, Select, MenuItem } from "@mui/material";
-import { Card, LineChart, Title, Grid, Metric } from "@tremor/react";
-import { XCircleIcon, PlusCircleIcon } from '@heroicons/react/solid'
+import { Button, Container, Select, MenuItem } from "@mui/material";
+import { Card, Grid } from "@tremor/react";
+import { PlusCircleIcon } from '@heroicons/react/solid'
 import 'tailwindcss/tailwind.css'
-import { useEffect, useState } from 'react';
-import Grid2 from '@mui/material/Unstable_Grid2'
+import { useState } from 'react';
 import Head from "next/head";
 import Link from "next/link";
-import useAnalytics from "@/client/hooks/useAnalytics";
 import { useRouter } from "next/router";
-import LoadingSpinner from "@/client/LoadingSpinner";
 import { UserRole } from "@/isaac/models/User";
 
 import ViewsOverTime from "./analytic_components/ViewsOverTime";
@@ -24,7 +21,12 @@ export default function Analytics() {
     const { page: title } = router.query;
 
     // TODO: get set widgets from user settings
-    const [testWidgets, setWidgets] = useState(["none", "eugh", "none", "none", "none", "none"] as string[]);
+    const [testWidgets, setWidgets] = useState(["none", "eugh", "eugh", "none", "eugh", "none"] as string[]);
+    const componentList = ["ViewsOverTime"];
+
+    const AddButton = () => {
+        // TODO: put add-widget card in here
+    }
 
     return (
         <>
@@ -33,11 +35,23 @@ export default function Analytics() {
             </Head>
             <Container>
                 <h1>{ `Page Analytics | ${title}` }</h1>
-
                 <Grid numColsMd={2} className="mt-6 gap-6">
                     {testWidgets.map((i) => {
                         if(i === "none") {
-                            return <Card key="add-widget"><PlusCircleIcon className="h-10 w-10 self-center" /></Card>
+                            return (
+                                <Card className="h-20" key="add-widget">
+                                    <div className="relative">
+                                        <PlusCircleIcon className="h-10 w-10 self-center absolute" onClick={addNewComponent} />
+                                        <Select className="opacity-0 absolute">
+                                            {componentList.map((i) => {
+                                                return (
+                                                    <MenuItem value={i} key={i}>{i}</MenuItem>
+                                                )
+                                            })}
+                                        </Select>
+                                    </div>
+                                </Card>
+                            );
                         } else {
                             return ViewsOverTime(title as string);
                         }
@@ -56,6 +70,7 @@ export default function Analytics() {
     )
 }
 
+// function to process metrics into easy to digest format
 export function processMetric(tempArr: MetricInterface[], metricName: any) {
     const indexCheck = tempArr.findIndex(e => e.name === metricName);
 
@@ -67,6 +82,10 @@ export function processMetric(tempArr: MetricInterface[], metricName: any) {
             value: 1
         });
     }
+}
+
+const addNewComponent = () => {
+
 }
 
 // ADMIN only
