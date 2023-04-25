122aeaa3753df5c2096bf56ad23ca06de643b340
treblenaX <elbertkcheng@outlook.com>
2023-04-25 11:59:45 -0700

    trending pages now returns a sub array of all pages if there are not enough pages with page clicks

--- pages/api/page/trending.ts
+++ pages/api/page/trending.ts
@@ -3,6 +3,9 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import '@/isaac/database/mongoose/MongooseProvider';
 import PublicAPIEndpoint from '@/isaac/public/PublicAPI';
 import { MetricPageClickAggType } from '@/isaac/public/api/MetricPageClick';
+import { GetPageTypes } from '@/isaac/public/api/Page';
+import { SortType } from '@/isaac/public/SortType';
+import { Page } from '@/isaac/models';
 
 const api = PublicAPIEndpoint;
 
@@ -14,7 +17,14 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     try {
         switch (method) {
             case 'GET':
+                const return_pages_amount = 5;
                 const aggregation: any[] = (await api.MetricPageClick.aggregate(MetricPageClickAggType.TRENDING_PAGES));
+
+                if (aggregation.length < return_pages_amount) {   // If there are less than 5 trending pages, append the most recent pages
+                    const recent_pages: Page[] = await api.Page.get(GetPageTypes.ALL_PAGES, SortType.RECENTLY_CREATED) as Page[];
+                    const diff = return_pages_amount - aggregation.length;
+                    aggregation.push(...recent_pages.slice(0, diff));
+                }
 
                 res.status(200).json({
                     success: true,
