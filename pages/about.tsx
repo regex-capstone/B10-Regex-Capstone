d30a3ba8c19a4d08728f3532746b9132e499eddd
treblenaX <elbertkcheng@outlook.com>
2023-05-03 11:36:34 -0700

    linting

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,10 +1,8 @@
 import "animate.css/animate.min.css";
 import Header from "@/client/Header";
 import Hero from "@/client/about/HeroComponent";
-import Team from "@/client/about/TeamComponent";
 import Why from "@/client/about/WhyComponent";
 import Footer from "@/client/about/FooterComponent";
-import Stakeholders from "@/client/about/StakeholderComponent";
 import Journey from "@/client/about/JourneyComponent";
 
 export default function About() {
